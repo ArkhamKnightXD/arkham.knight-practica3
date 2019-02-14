fd276247c478858f3c7c4273c64bbe2c7f7786fc
Ker <karvin_jimenez@hotmail.com>
2019-02-13 22:37:45 -0400

    Creacion de las clases propuesta para la tarea
    Ademas de que se le crearon sus respectivo constructores y getters and setters

--- src/main/java/arkham/knight/practica3/Main.java
+++ src/main/java/arkham/knight/practica3/Main.java
@@ -1,5 +1,7 @@
 package arkham.knight.practica3;
 
+import arkham.knight.practica3.encapsulacion.Usuario;
+
 public class Main {
     public static void main(String[] args) {
 
---
+++ src/main/java/arkham/knight/practica3/encapsulacion/Etiqueta.java
@@ -0,0 +1,30 @@
+package arkham.knight.practica3.encapsulacion;
+
+public class Etiqueta {
+    private long id;
+    private String etiqueta;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEtiqueta() {
+        return etiqueta;
+    }
+
+    public void setEtiqueta(String etiqueta) {
+        this.etiqueta = etiqueta;
+    }
+
+
+    public Etiqueta(long id, String etiqueta) {
+        this.id = id;
+        this.etiqueta = etiqueta;
+    }
+
+
+}
