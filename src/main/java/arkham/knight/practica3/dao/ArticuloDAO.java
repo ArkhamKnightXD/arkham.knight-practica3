ef696e4a645b7cb26644a2d7f49087493ee3d3e1
Ker <karvin_jimenez@hotmail.com>
2019-02-16 11:23:39 -0400

    agregando metodos a articuloDao

--- src/main/java/arkham/knight/practica3/dao/ArticuloDAO.java
+++ src/main/java/arkham/knight/practica3/dao/ArticuloDAO.java
@@ -2,5 +2,17 @@ package arkham.knight.practica3.dao;
 
 import arkham.knight.practica3.encapsulacion.Articulo;
 
+import java.util.List;
+
 public interface ArticuloDAO {
+
+    List<Articulo> listarArticulos();
+
+    Articulo encontrarPorId(int id);
+
+    void agregar(Articulo articulo);
+
+    void actualizar(Articulo articulo);
+
+    void borrar(Articulo articulo);
 }
